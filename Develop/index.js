a4d8aefbe914e3bdec1bcd4c1d2f34fc4403898e
Skronkie <ebrockway01@gmail.com>
2022-02-26 18:17:03 -0600

    added confirmation for contributers and validation for more input questions.

--- Develop/index.js
+++ Develop/index.js
@@ -4,18 +4,85 @@ const promptUser = () => {
   return inquirer.prompt([
     {
       type: "input",
-      name: "userName",
-      message: "whats ur name?",
-      validate: (userName) => {
-        if (userName) {
+      name: "title",
+      message: "What is the title of your project?",
+      validate: (title) => {
+        if (title) {
           return true;
         } else {
-          console.log("Please enter the name of your project");
+          console.log("Please enter the title of your project");
           return false;
         }
       },
     },
-    {},
+    {
+      type: "input",
+      name: "description",
+      message: "Write a description of your project.",
+      validate: (description) => {
+        if (description) {
+          return true;
+        } else {
+          console.log("Please enter a description for your project");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Write detailed instructions on how to install your project.",
+      validate: (installation) => {
+        if (installation) {
+          return true;
+        } else {
+          console.log("Please enter detailed instructions for your project");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Write information on how to use the application ",
+      validate: (usage) => {
+        if (usage) {
+          return true;
+        } else {
+          console.log(
+            "Please enter information on how to use the application "
+          );
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmContribution",
+      message: "Did anyone help you with your project?",
+      default: true,
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Write the people who contributed to your project.",
+      when: ({ confirmContribution }) => {
+        if (confirmContribution) {
+          return true;
+        }
+        return false;
+      },
+      validate: (contribution) => {
+        if (contribution) {
+          return true;
+        } else {
+          console.log(
+            "Dont forget to add the people who helped with your project!"
+          );
+          return false;
+        }
+      },
+    },
   ]);
 };
 promptUser();
