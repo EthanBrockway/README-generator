339a26c33f9fed423a2e5efd5a8ce1029df674cc
Skronkie <ebrockway01@gmail.com>
2022-02-27 15:49:05 -0600

    added renderLicenseLink function

--- Develop/dist/README.md
+++ Develop/dist/README.md
@@ -1,5 +1,18 @@
 
+ [![MIT,GNU,Eclipse,Mozilla,Apache,Oracle](https://img.shields.io/badge/license-MIT,GNU,Eclipse,Mozilla,Apache,Oracle-green)](https://img.shields.io/badge/license-MIT,GNU,Eclipse,Mozilla,Apache,Oracle-green)
   # dsa
+
+  # Table of contents
+
+  [Description](#Description)
+  [Installation](Installation)
+  [Usage](#Usage)
+  [Contributors](#Contributors)
+  [Testing](#Testing)
+
+  ## License
+
+  [MIT](https://choosealicense.com/licenses/mit/)  [GNU](https://choosealicense.com/licenses/agpl-3.0/)  [Eclipse](https://www.eclipse.org/legal/epl-2.0/)  [Mozilla](https://www.mozilla.org/en-US/MPL/2.0/)  [Mozilla](https://www.mozilla.org/en-US/MPL/2.0/)  
 
   ## Description
 
@@ -13,6 +26,10 @@
 
   dsa
 
+  ## Contributors
+
+   noah vandy
+
   ## Testing
-  
+
    dsa
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `[![${license}](https://img.shields.io/badge/license-${license}-green)](https://img.shields.io/badge/license-${license}-green)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenses) {
+  let badges = [];
+
+  if (licenses.length === 0) {
+    return "";
+  }
+  licenses.forEach((license) => {
+    if (license === "MIT") {
+      badges.push("[MIT](https://choosealicense.com/licenses/mit/)");
+    }
+    if (license === "GNU") {
+      badges.push("[GNU](https://choosealicense.com/licenses/agpl-3.0/)");
+    }
+    if (license === "Eclipse") {
+      badges.push("[Eclipse](https://www.eclipse.org/legal/epl-2.0/)");
+    }
+    if (license === "Mozilla") {
+      badges.push("[Mozilla](https://www.mozilla.org/en-US/MPL/2.0/)");
+    }
+    if (license === "Apache") {
+      badges.push("[Mozilla](https://www.mozilla.org/en-US/MPL/2.0/)");
+    }
+  });
+
+  let licenseString = "";
+  badges.forEach((badge) => (licenseString += badge + "\n"));
+  return licenseString;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +46,20 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
+ ${renderLicenseBadge(data.license)}
   # ${data.title}
+
+  # Table of contents
+
+  [Description](#Description)
+  [Installation](Installation)
+  [Usage](#Usage)
+  [Contributors](#Contributors)
+  [Testing](#Testing)
+
+  ## License
+
+  ${renderLicenseLink(data.license)}
 
   ## Description
 
@@ -40,6 +86,13 @@ function generateMarkdown(data) {
 function noContribution(data) {
   return `
   # ${data.title}
+
+  # Table of contents
+
+  [Description](#Description)
+  [Installation](Installation)
+  [Usage](#Usage)
+  [Testing](#Testing)
 
   ## Description
 
